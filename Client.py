c3323d9849af4f325378e5f04ea366b7d9436b6a
Rodrigo de Sapienza Luna <rodrigo.luna@outlook.com.br>
2020-03-02 21:20:50 -0300

    Adicionando funcionalidades a serem descritas num futuro commit

---
+++ Client.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import socket   
+#Com essa lib eu consigo realizar a conexao ao servidor FTP
+import ftplib
+
+def main():
+
+    ftp = ftplib.FTP()
+    
+
+    IPAddr = sys.argv[1]
+    port = 2121
+
+    
+    path = sys.argv[2]
+    filename = sys.argv[3]
+    ftp.connect(IPAddr, port)
+    ftp.login('user','12345')
+
+    #Configura o diretorio que se quer obter as informacoes
+    # print(conn.dir("/home/rodrigoluna"))
+
+    file = open(path+"/"+filename, 'wb')
+    
+    ftp.retrbinary("RETR " + 'files/' +filename, file.write)
+    #Retorna os nomes dos arquivos
+    
+if __name__ == '__main__':
+    main()
